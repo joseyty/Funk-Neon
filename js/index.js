9882625987279a230fc7a57412ec194943f4afba
Igor jose <lmelo7262@gmail.com>
2026-08-16 00:31:33 -0300

    inicio

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function clicarButton(){
+alert("fucionou")
+
+}
+
+function clicarButton(){
+let email= documentgetElementById("email").
+let  senha= documentgetElementById("email").
+
+
+if(senha=="". email==""){
+    alert("Preencha")
+    alert.style.color="red"
+}
+
+
+}
