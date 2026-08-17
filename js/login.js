abcefdab6d706cee7646cb334296fb1de3417dc9
Igor jose <lmelo7262@gmail.com>
2026-08-17 17:13:13 -0300

    update

---
+++ js/login.js
@@ -0,0 +1,17 @@
+function clicarButton(){
+alert("fucionou")
+
+}
+
+function clicarButton(){
+let codiNome= document.getElementById("codiNome").value;
+let  senha= document.getElementById("senha").value;
+
+
+if(senha=="" || codiNome==""){
+    alert("Preencha todos os campos")
+}
+    alert.style.color="red"
+}
+
+
