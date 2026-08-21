071b4cc88686bfe0d967e7da57838ccea003733c
Igor jose <lmelo7262@gmail.com>
2026-08-21 00:30:25 -0300

    ajustes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-function iniciarMusica(){
 
-document.getElementById("musica1").play();
 
+
+let musicaAtual = 1;
+
+function iniciarMusica() {
+    tocarMusica("musica1");
+}
+
+function tocarMusica(id) {
+    let musica = document.getElementById(id);
+
+    musica.play();
+
+    musica.onended = function () {
+        musicaAtual++;
+
+        if (musicaAtual <= 3) {
+            tocarMusica("musica" + musicaAtual);
+        }
+    };
+}
+
+function pausarMusica() {
+    document.getElementById("musica" + musicaAtual).pause();
 }
